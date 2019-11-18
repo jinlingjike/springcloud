ce6558c622f595ad9921b405417aacd911f8ca0d
jiapeng <jpzhang@akazam.com>
2019-11-18 16:42:14 +0800

    初始化

---
+++ serice-feign/src/main/java/com/gapon/sericefeign/service/SchedualServiceHi.java
@@ -0,0 +1,13 @@
+package com.gapon.sericefeign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+@Repository
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ service-hi/src/test/java/com/gapon/servicehi/ServiceHiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gapon.servicehi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceHiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
