781b19b8ef7483f4891cf15ed450e8d1de573db7
jiapeng <jpzhang@akazam.com>
2019-11-19 09:35:56 +0800

    新增配置

--- config-server/src/main/java/com/gapon/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/gapon/configserver/ConfigServerApplication.java
@@ -1,8 +1,10 @@
 package com.gapon.configserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +12,14 @@ public class ConfigServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
 
 }
