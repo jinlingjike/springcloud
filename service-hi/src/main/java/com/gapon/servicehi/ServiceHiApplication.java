140d0a38455724f345d788198a0f2a21ed0525f4
jiapeng <jpzhang@akazam.com>
2019-11-19 15:26:55 +0800

    新增配置

--- service-hi/src/main/java/com/gapon/servicehi/ServiceHiApplication.java
+++ service-hi/src/main/java/com/gapon/servicehi/ServiceHiApplication.java
@@ -1,11 +1,16 @@
 package com.gapon.servicehi;
 
 import brave.sampler.Sampler;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,7 +22,11 @@ import java.util.logging.Logger;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceHiApplication {
     private static final Logger LOG = Logger.getLogger(ServiceHiApplication.class.getName());
 
@@ -41,6 +50,7 @@ public class ServiceHiApplication {
     }
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String callHome(){
         LOG.log(Level.INFO, "calling trace service-hi  ");
         return restTemplate.getForObject("http://localhost:8989/miya", String.class);
@@ -56,6 +66,9 @@ public class ServiceHiApplication {
     @Bean
     public Sampler defaultSampler() {
         return Sampler.ALWAYS_SAMPLE;
+    }
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 
 
