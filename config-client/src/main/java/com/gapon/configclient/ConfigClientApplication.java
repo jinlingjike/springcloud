e0196f4761a960ece5ac0680a507021a60493b6d
jiapeng <jpzhang@akazam.com>
2019-11-19 10:08:58 +0800

    新增配置

--- config-client/src/main/java/com/gapon/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/gapon/configclient/ConfigClientApplication.java
@@ -1,7 +1,9 @@
 package com.gapon.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,5 +13,12 @@ public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+
 
 }
